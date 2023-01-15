9055dc731970460157892544b164f5966bb90c9b
sschr15 <steven.sschr15@hotmail.com>
2023-01-15 00:36:21 -0600

    add big test example code thing

--- src/linuxX64Main/kotlin/linux/x64/LinuxX64Main.kt
+++ src/linuxX64Main/kotlin/linux/x64/LinuxX64Main.kt
@@ -6,6 +6,11 @@ import kotlinx.cinterop.ptr
 import kotlinx.cinterop.readBytes
 import platform.posix.*
 
+val LFLAGS_TO_DISABLE = listOf(
+    ICANON,
+    ECHO,
+).map { it.toUInt().inv() }.reduce { acc, i -> acc and i }
+
 // kt/n is a mess ngl
 fun main() {
     if (isatty(fileno(stdin)) != 0) {
@@ -20,19 +25,50 @@ fun main() {
         // trading card game extra terrestrial attribute
         // :iea:
         tcgetattr(fileno(stdin), termios.ptr)
-        termios.c_lflag = termios.c_lflag and ICANON.toUInt().inv()
+        termios.c_lflag = termios.c_lflag and LFLAGS_TO_DISABLE
         tcsetattr(fileno(stdin), TCSANOW, termios.ptr)
     }
 
+    val exampleList = listOf(
+        "hello",
+        "world",
+        "this",
+        "is",
+        "a",
+        "test",
+    )
+
     // oh god unsafe code
     val singleByteArray = malloc(1)!!
-    var bytesRead = read(fileno(stdin), singleByteArray, 1)
     val input = mutableListOf<UByte>()
-    while (bytesRead > 0) {
-        bytesRead = read(fileno(stdin), singleByteArray, 1)
+    var currentItem = 0
+    while (true) {
+        read(fileno(stdin), singleByteArray, 1)
         val byte = singleByteArray.readBytes(1)[0].toUByte()
-        if (byte == 4.toUByte()) break
-        input.add(byte)
+        when (byte.toInt().toChar()) {
+            '\u0004' -> break // ctrl+d
+            '\t' -> {
+                print("\u001b[2K\r") // clear line
+                val item = exampleList[currentItem]
+                print(exampleList[currentItem++]) // print item
+                currentItem %= exampleList.size // wrap around
+                input.clear() // clear input
+                input.addAll(item.map { it.code.toUByte() }) // add item to input
+            }
+            '\u0008', '\u007f' -> {
+                if (input.isNotEmpty()) {
+                    print("\u001b[1D") // move cursor left
+                    print("\u001b[2K\r") // clear line
+                    print(input.dropLast(1).map { it.toInt().toChar() }.joinToString("")) // print input
+                    input.removeLast() // remove last char
+                }
+            }
+            else -> {
+                input.add(byte)
+                print(byte.toInt().toChar())
+            }
+        }
     }
     println(input)
+    println("(the input was: ${input.map { it.toInt().toChar() }.joinToString("").trimEnd()})")
 }
